7dfdb55bc59d4036da5724459b08fa2558bdaaf4
Ghana Bambale <ghanshyam.moscot@gmail.com>
2021-07-09 01:20:29 +0530

    initialize driver and open url code added

---
+++ code/base_code.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+#driver = webdriver.Firefox(executable_path="/home/thebadcoder/MyWorkSpace/Testing/python_selenium/code/drivers/geckodriver-v0.29.1-linux32/geckodriver")
+driver = webdriver.Firefox()
+driver.get("https://www.google.com")
+
