c9b82089f9ea23578a98dd32473ad6db7660db80
Ghana Bambale <ghanshyam.moscot@gmail.com>
2021-07-10 01:41:45 +0530

    wait for element functionality code added

--- code/base_code.py
+++ code/base_code.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 #driver = webdriver.Firefox(executable_path="/home/thebadcoder/MyWorkSpace/Testing/python_selenium/code/drivers/geckodriver-v0.29.1-linux32/geckodriver")
 driver = webdriver.Firefox()
-driver.get("https://www.amazon.in")
+url = "https://www.amazon.in"
+driver.get(url)
 
 
 ## Validation
@@ -9,14 +15,27 @@ page_title = driver.title
 print("Page title: ",page_title)
 
 ## input something
-search_for = "OnePlus Nord CE 5G"
+search_for_product = "OnePlus Nord CE 5G"
 
 search_box = driver.find_element_by_xpath('//input[@id="twotabsearchtextbox"]')
-search_box.send_keys(search_for)
+search_box.send_keys(search_for_product)
 
 ## click something
 search_button = driver.find_element_by_id("nav-search-submit-button")
 search_button.click()
+
+## Wait for Search results
+wait_for_serch_element = "a-section a-spacing-small a-spacing-top-small"
+try:
+    element_to_expect = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"//div[@class='a-section a-spacing-small a-spacing-top-small']")))
+
+except:
+    print("There was an exception")
+
+## Name to check in list and click on
+## OnePlus Nord CE 5G (Charcoal Ink, 8GB RAM, 128GB Storage)
+product_name = "OnePlus Nord CE 5G (Charcoal Ink, 8GB RAM, 128GB Storage)"
+
 
 print("Automation completed")
 driver.close()
